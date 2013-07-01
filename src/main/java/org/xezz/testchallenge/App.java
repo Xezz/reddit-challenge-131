e706d4bea567882d32095c702f5a70a52426dc7e
Xezz <koch@xezz.org>
2013-07-01 14:57:06 +0200

    REFACTOR: Moved parsing and result reporting into its own method

--- src/main/java/org/xezz/testchallenge/App.java
+++ src/main/java/org/xezz/testchallenge/App.java
@@ -1,21 +1,60 @@
 package org.xezz.testchallenge;
 
-import java.util.Scanner;
+import org.xezz.testchallenge.parsing.InputParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 /**
  * Solution to the Reddit Challange 131
  */
 public class App {
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        final int amount = scanner.nextInt();
-        for (int i = 0; i < amount; i++) {
-            if (InputTester.testData(TestType.getTypeByValue(scanner.nextInt()), scanner.next(), scanner.next())) {
-                System.out.println("Good test data");
-            } else {
-                System.out.println("Mismatch! Bad test data");
+        InputParser.handleInput(System.in, System.out);
+        //readFromFile();
+    }
+
+    /**
+     * Example how to use files as input/output
+     */
+    private static void readFromFile() {
+        InputStream fileInputStream = null;
+        OutputStream fileOutputStream = null;
+        final String workingDirectory = System.getProperty("user.dir");
+        final String inputFileName = "text.txt";
+        final String outputFileName = "result.txt";
+        final Path path = Paths.get(workingDirectory);
+        try {
+            fileOutputStream = Files.newOutputStream(path.resolve(outputFileName), StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.APPEND);
+            fileInputStream = Files.newInputStream(path.resolve(inputFileName));
+            if (fileInputStream != null && fileOutputStream != null) {
+                InputParser.handleInput(fileInputStream, new PrintStream(fileOutputStream));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    System.err.println("Could not close Input stream");
+                    e.printStackTrace();
+                }
+            }
+            if (fileOutputStream != null) {
+                try {
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    System.err.println("Could not close Output stream");
+                    e.printStackTrace();
+                }
             }
         }
     }
-
 }
---
+++ src/main/java/org/xezz/testchallenge/parsing/InputParser.java
@@ -0,0 +1,39 @@
+package org.xezz.testchallenge.parsing;
+
+import org.xezz.testchallenge.InputTester;
+import org.xezz.testchallenge.TestType;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/**
+ * User: Xezz
+ * Date: 01.07.13
+ * Time: 13:38
+ */
+public final class InputParser {
+    /**
+     * Do not allow any instance of this class to be created
+     */
+    private InputParser() {
+    }
+
+    /**
+     * Reads input and writes the result to the output
+     *
+     * @param inputStream  input to be read from
+     * @param outputStream output to write to
+     */
+    public static void handleInput(final InputStream inputStream, final PrintStream outputStream) {
+        final Scanner scanner = new Scanner(inputStream);
+        final int amount = scanner.nextInt();
+        for (int i = 0; i < amount; i++) {
+            if (InputTester.testData(TestType.getTypeByValue(scanner.nextInt()), scanner.next(), scanner.next())) {
+                outputStream.println("Good test data");
+            } else {
+                outputStream.println("Mismatch! Bad test data");
+            }
+        }
+    }
+}
