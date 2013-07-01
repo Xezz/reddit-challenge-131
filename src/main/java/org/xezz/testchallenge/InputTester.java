efc6f38f0e03aa523f8154e1ece4b864e18a5de5
Xezz <koch@xezz.org>
2013-07-01 12:00:07 +0200

    UPDATE: JavaDoc

--- src/main/java/org/xezz/testchallenge/InputTester.java
+++ src/main/java/org/xezz/testchallenge/InputTester.java
@@ -6,7 +6,16 @@ import org.xezz.testchallenge.tester.TesterBuilder;
  * Test a given input and the expected result on validity
  */
 public final class InputTester {
-    public static boolean testData(final TestType testType, final String input, final String result) {
+    /**
+     * Test input on validity depending on the type of the test
+     *
+     * @param testType operation to test for
+     * @param input    String to transform
+     * @param result   result of the transformation
+     * @return true when the result is correct for the input and the TestType
+     * @throws UnsupportedOperationException if no implementation for the TestType is available
+     */
+    public static boolean testData(final TestType testType, final String input, final String result) throws UnsupportedOperationException {
         return TesterBuilder.buildTester(testType).testInput(input, result);
     }
 }
--- src/main/java/org/xezz/testchallenge/tester/TesterBuilder.java
+++ src/main/java/org/xezz/testchallenge/tester/TesterBuilder.java
@@ -6,16 +6,24 @@ import org.xezz.testchallenge.TestType;
  * User: Xezz
  * Date: 01.07.13
  * Time: 11:34
+ * Builds Testable depending on the TestType
  */
 public final class TesterBuilder {
-    public static Testable buildTester(TestType type) {
+    /**
+     * Builds a suited implementation of Testable
+     *
+     * @param type that the Testable has to support
+     * @return fitting Testable to the TestType
+     * @throws UnsupportedOperationException if there is no fitting implementation available
+     */
+    public static Testable buildTester(TestType type) throws UnsupportedOperationException {
         switch (type) {
             case REVERSE:
                 return new ReverseTester();
             case UPPERCASE:
                 return new UpperCaseTester();
             case LOWERCASE:
-                // return new LowerCaseTester();
+                return new LowerCaseTester();
             default:
                 throw new UnsupportedOperationException("No implementation know for TestType: " + type.name());
         }
