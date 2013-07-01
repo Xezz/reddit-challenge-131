6caabb78fd9d981dd1e9a2d141b217923845f75a
Xezz <koch@xezz.org>
2013-07-01 13:15:53 +0200

    NEW: Unit tests of ReverseValidator

--- src/test/java/org/xezz/testchallenge/validation/ReverseValidatorTest.java
+++ src/test/java/org/xezz/testchallenge/validation/ReverseValidatorTest.java
@@ -1,6 +1,9 @@
 package org.xezz.testchallenge.validation;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
 
 /**
  * User: Xezz
@@ -9,10 +12,26 @@ import org.junit.Test;
  */
 public class ReverseValidatorTest {
     private static final String INPUT = "AccFggT";
+    private static final String VALID_RESULT = "TggFccA";
     private final ReverseValidator validator = new ReverseValidator();
 
     @Test
     public void testValidate() throws Exception {
+        Assert.assertThat("Reversed String didn't match", true, is(validator.validate(INPUT, VALID_RESULT)));
+    }
 
+    @Test
+    public void testValidateSameStringNonReversed() throws Exception {
+        Assert.assertThat("Same String matched", false, is(validator.validate(INPUT, INPUT)));
+    }
+
+    @Test
+    public void testValidateShorterStringNotMatching() throws Exception {
+        Assert.assertThat("Shorter String matched", false, is(validator.validate(INPUT, VALID_RESULT.substring(2))));
+    }
+
+    @Test
+    public void testValidateLongerStringNotMatching() throws Exception {
+        Assert.assertThat("Longer String matched", false, is(validator.validate(INPUT, VALID_RESULT + VALID_RESULT)));
     }
 }
