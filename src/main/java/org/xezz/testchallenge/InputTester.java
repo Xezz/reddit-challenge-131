3eeb875a36233ecd9f09160fe4dfb1b1bd8def65
Xezz <koch@xezz.org>
2013-07-01 11:53:42 +0200

    UPDATE: Implementation of InputTester

--- src/main/java/org/xezz/testchallenge/InputTester.java
+++ src/main/java/org/xezz/testchallenge/InputTester.java
@@ -5,16 +5,8 @@ import org.xezz.testchallenge.tester.TesterBuilder;
 /**
  * Test a given input and the expected result on validity
  */
-public class InputTester {
+public final class InputTester {
     public static boolean testData(final TestType testType, final String input, final String result) {
         return TesterBuilder.buildTester(testType).testInput(input, result);
-    }
-
-    static boolean testReverseString(final String input, final String result) {
-        return result.equals(new StringBuilder(input).reverse().toString());
-    }
-
-    static boolean testUpperCaseString(final String input, final String result) {
-        return result.equals(input.toUpperCase());
     }
 }
